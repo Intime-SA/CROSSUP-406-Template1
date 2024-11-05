a0ad1315ab73e57812d1cf00b7ea4ebdb164c833
Intime-SA <invertimesa@gmail.com>
2024-11-05 17:21:23 -0300

    correccion templates deletes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 
 import React from "react";
 import Template1A from "@/components/template1/template1A";
-import Template1B from "@/components/template1/template1B";
-import Template1C from "@/components/template1/template1C";
-import Template1D from "@/components/template1/template1D";
 
 const TemplateMobilePage = () => {
   return (
@@ -12,9 +9,6 @@ const TemplateMobilePage = () => {
       <h1 className="text-3xl font-bold mb-4">Template 1</h1>
       <h2 className="text-sm mb-4">Variantes de template</h2>
       <Template1A />
-      {/*       <Template1B />
-      <Template1C />
-      <Template1D /> */}
     </div>
   );
 };
