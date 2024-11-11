c95e08868cc13f665f9aa5b2222feb80b2c765a5
Intime-SA <invertimesa@gmail.com>
2024-11-11 11:57:20 -0300

    desing type!

--- src/data/data3.ts
+++ src/data/data3.ts
@@ -1,4 +1,4 @@
-import { PromotionData } from "@/domain/definitionsTypes";
+import { DesignType, PromotionData } from "@/domain/definitionsTypes";
 
 export const getMockOfferData3 = (): PromotionData => ({
   _id: "67223aac87729c4433dcbc31",
@@ -7,6 +7,7 @@ export const getMockOfferData3 = (): PromotionData => ({
     secondary: "grey",
     font: "Lato",
   },
+  desingType: DesignType.VERTICAL,
   targets: [
     {
       _id: "64de16e59ffb22f98387cc3c",
