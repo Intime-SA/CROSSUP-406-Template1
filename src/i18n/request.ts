810c3aab1577d916e78a0f205d99a67629898c13
Intime-SA <invertimesa@gmail.com>
2024-11-20 13:03:46 -0300

    cambios interfaz

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,14 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
+type SupportedLocale = "es" | "pt";
+
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
   // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (!locale || !routing.locales.includes(locale as SupportedLocale)) {
     locale = routing.defaultLocale;
   }
 
