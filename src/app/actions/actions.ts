071fc2b213fa94b5d0ccf0218feb547d345295a0
Intime-SA <invertimesa@gmail.com>
2024-12-10 14:31:08 -0300

    sin server actions

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,7 +1,4 @@
-"use server";
-
-// mock de data API CROSS UP > SUGERENCIA / RECOMENDACION
-import { getMockOfferData } from "@/data/data";
+/* import { getMockOfferData } from "@/data/data";
 import { getMockOfferData2 } from "@/data/data2";
 import { getMockOfferData3 } from "@/data/data3";
 import { getMockOfferData4 } from "@/data/data4";
@@ -32,3 +29,4 @@ export async function fetchDataFromJson(typeTemplate: string) {
     throw new Error("Failed to fetch data");
   }
 }
+ */
