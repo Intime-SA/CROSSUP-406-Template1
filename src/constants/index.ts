4e8a8c9bed3b26182956db4090b6f6822db4baff
Intime-SA <invertimesa@gmail.com>
2024-12-10 13:31:56 -0300

    cambios

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,7 @@ export const DEFAULT_MODE = "fixed";
 export const PREVIEW_MODES = ["fixed"];
 export const FIXED_MODES = ["fixed", "admin"];
 
+//as
 //Text constants
 export const MAX_TITLE_LENGTH = 50;
 export const MAX_DESCRIPTION_LENGTH = 150;
